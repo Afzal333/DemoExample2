ed5acde3eec36734c298d16148a59e701d221bd4
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-02 09:23:38 +0530

    [Afzal] Added: PrimeNumber

---
+++ DemoWxample2/DemoWxample2/PrimeNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoExample2
+{
+    public class PrimeNumber
+    {
+        public void CheckPrime(int num)
+        {
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if(num % i == 0)
+                    count++;
+            }
+            if (count == 2)
+                Console.WriteLine("Number is a Prime Number");
+            else
+                Console.WriteLine("Num is not a prime number");
+
+        }
+    }
+        
+}
--- DemoWxample2/DemoWxample2/Program.cs
+++ DemoWxample2/DemoWxample2/Program.cs
@@ -9,7 +9,8 @@ namespace DemoExample2
             Console.WriteLine("Demo Example 2"+ "\n\n");
             Console.WriteLine( "Enter the option");
             Console.WriteLine("1.FibonacciSeries"+ "\n" +
-                                "2.PerfectNumber"+"\n");
+                                "2.PerfectNumber"+"\n"+
+                                "3.PrimeNumber"+"\n");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
@@ -25,6 +26,12 @@ namespace DemoExample2
                     Console.WriteLine("Enter the Value"+"\n\n");
                     int value = Convert.ToInt32(Console.ReadLine());
                     perfectNumber.Check(value);
+                    break;
+                case 3:
+                    PrimeNumber primeNumber = new PrimeNumber();                    
+                    Console.WriteLine("Enter the Value to check Prime" + "\n\n");
+                    int value1 = Convert.ToInt32(Console.ReadLine());
+                    primeNumber.CheckPrime(value1);
                     break;
             }
         }
