c04d9abfd0470159b19c2bea5d15c0d803f6f9ac
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-02 09:14:36 +0530

    [Afzal] Add: Perfect Number

---
+++ DemoWxample2/DemoWxample2/PerfectNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoExample2
+{
+    public class PerfectNumber
+    {
+        public void Check(int num)
+        {
+            int sum=0;
+            for(int i = 1; i <= num / 2; i++)
+            {
+                if(num % i == 0)
+                {
+                    sum+=i;
+                }
+            }
+            if(num==sum)
+                Console.WriteLine("Number provided ia an PerfectNumber ");
+            else
+                Console.WriteLine("Not an PerfectNumber");
+        }
+    }
+}
--- DemoWxample2/DemoWxample2/Program.cs
+++ DemoWxample2/DemoWxample2/Program.cs
@@ -8,7 +8,8 @@ namespace DemoExample2
         {
             Console.WriteLine("Demo Example 2"+ "\n\n");
             Console.WriteLine( "Enter the option");
-            Console.WriteLine("1.FibonacciSeries"+ "\n");
+            Console.WriteLine("1.FibonacciSeries"+ "\n" +
+                                "2.PerfectNumber"+"\n");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
@@ -17,6 +18,13 @@ namespace DemoExample2
                     Console.WriteLine("\n\n"+"Enter the FibonacciSeries Value"+"\n");
                     int range = Convert.ToInt32(Console.ReadLine());
                     series.Print(range);
+                    break;
+                case 2:
+                    PerfectNumber perfectNumber = new PerfectNumber();
+                    Console.WriteLine("\n\n" + "Check the Number Perfect or Not" + "\n");
+                    Console.WriteLine("Enter the Value"+"\n\n");
+                    int value = Convert.ToInt32(Console.ReadLine());
+                    perfectNumber.Check(value);
                     break;
             }
         }
