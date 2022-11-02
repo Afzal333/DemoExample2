2f23f768f1910c96458222ccc15fb584d4667ffc
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-02 08:52:26 +0530

    [Afzal] Added : FibonacciSeries

---
+++ DemoWxample2/DemoWxample2/FibonacciSeries.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoExample2
+{
+    public class FibonacciSeries
+    {
+        public void Print (int num)
+        {
+            int firstNum = 0, secondNum = 1, thirdNum = 0;
+            Console.WriteLine("\n\n"+firstNum+"\n"+secondNum);
+            for(int i = 0; i < num; i++)
+            {
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                Console.WriteLine(thirdNum);
+            }
+        }
+    }
+}
--- DemoWxample2/DemoWxample2/Program.cs
+++ DemoWxample2/DemoWxample2/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace DemoWxample2
+namespace DemoExample2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Demo Example 2"+ "\n\n");
+            Console.WriteLine( "Enter the option");
+            Console.WriteLine("1.FibonacciSeries"+ "\n");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    FibonacciSeries series = new FibonacciSeries();
+                    Console.WriteLine("\n\n"+"Enter the FibonacciSeries Value"+"\n");
+                    int range = Convert.ToInt32(Console.ReadLine());
+                    series.Print(range);
+                    break;
+            }
         }
     }
 }
