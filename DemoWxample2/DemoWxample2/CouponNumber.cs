2faf3a5455df216974c11f52fa732245ecefd13b
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-03 09:37:44 +0530

    [Afzal] Added: CouponNumber

---
+++ DemoWxample2/DemoWxample2/CouponNumber.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoExample2
+{
+    public class CouponNumber
+    {
+        public static int GenerateRandom()
+        {
+            Random random = new Random();
+            int randomNum = random.Next(0, 11);
+            return randomNum;
+
+        }
+        public static void GenerateCoupons(int num)
+        {
+            int[] arr = new int[num];
+            for(int i=0; i < arr.Length; i++)
+            {
+                int count = 0;
+                int random = GenerateRandom();                
+                for(int j = 0; j <= i; j++)
+                {
+                    if(arr[j] == random)
+                    {
+                        count++;    
+                    }
+                }
+                if(count == 0)
+                {
+                    arr[i] = random;
+                }
+
+            }
+            foreach(var data in arr)
+            {
+                if (data != 0)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+
+
+        }
+    }
+}
+
+//foreach(var data in arr)
+//{
+//    if(data == random)
+//    {
+//        return;
+//    }
+//}
--- DemoWxample2/DemoWxample2/Program.cs
+++ DemoWxample2/DemoWxample2/Program.cs
@@ -11,34 +11,40 @@ namespace DemoExample2
             Console.WriteLine("1.FibonacciSeries"+ "\n" +
                                 "2.PerfectNumber"+"\n"+
                                 "3.PrimeNumber"+"\n"+
-                                "4.ReverseNumber");
+                                "4.ReverseNumber"+"\n"+
+                                "5.CouponNumber"+"\n");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
                 case 1:
                     FibonacciSeries series = new FibonacciSeries();
-                    Console.WriteLine("\n\n"+"Enter the FibonacciSeries Value"+"\n");
+                    Console.WriteLine("\n"+"Enter the FibonacciSeries Value"+"\n");
                     int range = Convert.ToInt32(Console.ReadLine());
                     series.Print(range);
                     break;
                 case 2:
                     PerfectNumber perfectNumber = new PerfectNumber();
-                    Console.WriteLine("\n\n" + "Check the Number Perfect or Not" + "\n");
+                    Console.WriteLine("\n" + "Check the Number Perfect or Not" + "\n");
                     Console.WriteLine("Enter the Value"+"\n\n");
                     int value = Convert.ToInt32(Console.ReadLine());
                     perfectNumber.Check(value);
                     break;
                 case 3:
                     PrimeNumber primeNumber = new PrimeNumber();                    
-                    Console.WriteLine("Enter the Value to check Prime" + "\n\n");
+                    Console.WriteLine("\n" + "Enter the Value to check Prime" + "\n");
                     int value1 = Convert.ToInt32(Console.ReadLine());
                     primeNumber.CheckPrime(value1);
                     break;
                 case 4:
                     ReverseNumber reverseNumber = new ReverseNumber();
-                    Console.WriteLine("Enter the Value to Reverse" + "\n\n");
+                    Console.WriteLine("\n" + "Enter the Value to Reverse" + "\n");
                     int value2 = Convert.ToInt32(Console.ReadLine());
                     reverseNumber.Reverse(value2);
+                    break;
+                case 5:                    
+                    Console.WriteLine("\n" + "Enter the no: of Coupons" +"\n");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    CouponNumber.GenerateCoupons(num);
                     break;
 
             }
